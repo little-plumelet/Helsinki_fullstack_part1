0c70da3f8d7b36029faff77e5383e91f8f5458bf
Anastasiia Perova <perova@MacBook-Pro-Anastasiia.local>
2023-02-17 12:28:34 +0400

    add routes for exercises in part_1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { CourseinfoPage } from './courseinfo/CourceInfoPage';
 import { UniCafePage } from './unicafe/UniCafePage';
-
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+ } from 'react-router-dom';
+ import React from 'react';
+ 
 const App = () => {
-  
-
-  
 
   return (
-    <div>
-      <UniCafePage />
-      <CourseinfoPage />
-    </div>
+    <Router>
+      <div>
+        <ul style={{display: 'flex', gap: '20px'}}>
+          <li style={{listStyleType: 'none'}}><Link to='/courseinfo'>courseinfo</Link></li>
+          <li style={{listStyleType: 'none'}}><Link to='/unicafe'>unicafe</Link></li>
+        </ul>
+      </div>
+      <Routes>
+        <Route exact path='/courseinfo' element={<CourseinfoPage />} />
+        <Route exact path='/unicafe' element={<UniCafePage />} />
+      </Routes>
+    </Router>
   )
 }
 
