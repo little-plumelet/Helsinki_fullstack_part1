089894c16a9d72807dacf8917b416938fefafc1b
Anastasiia Perova <perova@MacBook-Pro-Anastasiia.local>
2023-02-17 18:20:40 +0400

    finish task 1.8

---
+++ src/unicafe/Statistics.js
@@ -0,0 +1,24 @@
+import { Header } from "./Header";
+
+export const Statistics = ({
+  good,
+  bad,
+  neutral,
+  goodTitle,
+  neutralTitle,
+  badTitle,
+}) => {
+  return (
+    <>
+      <Header title={"Statistics"} />
+      <ul>
+        <li>{`${goodTitle}: ${good}`}</li>
+        <li>{`${neutralTitle}: ${neutral}`}</li>
+        <li>{`${badTitle}: ${bad}`}</li>
+        <li>{`all: ${good + bad + neutral}`}</li>
+        <li>{`average: ${(good - bad) / (good + bad + neutral)}`}</li>
+        <li>{`positive: ${good / ((good + bad + neutral) / 100)}%`}</li>
+      </ul>
+    </>
+  );
+};
--- src/unicafe/UniCafePage.js
+++ src/unicafe/UniCafePage.js
@@ -1,33 +1,40 @@
-import { useState } from 'react';
-import { Button } from './Button';
-import { Header } from './Header';
+import { useState } from "react";
+import { Button } from "./Button";
+import { Header } from "./Header";
+import { Statistics } from "./Statistics";
 
 export const UniCafePage = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const goodTitle = 'good';
-  const neutralTitle = 'neutral';
-  const badTitle = 'bad';
+  const goodTitle = "good";
+  const neutralTitle = "neutral";
+  const badTitle = "bad";
 
   return (
     <section>
-        <Header title={'Give feedback'} />
-        <Button title={goodTitle} handleClick={() => setGood((prevState) => prevState + 1)}/>
-        <Button title={neutralTitle} handleClick={() => setNeutral((prevState) => prevState + 1)}/>
-        <Button title={badTitle} handleClick={() => setBad((prevState) => prevState + 1)}/>
-        <section>
-          <Header title={'Statistics'} />
-          <ul>
-            <li>{`${goodTitle}: ${good}`}</li>
-            <li>{`${neutralTitle}: ${neutral}`}</li>
-            <li>{`${badTitle}: ${bad}`}</li>
-            <li>{`all: ${(good + bad + neutral)}`}</li>
-            <li>{`average: ${(good - bad) / (good + bad + neutral) }`}</li>
-            <li>{`positive: ${good / ((good + bad + neutral) / 100) }%`}</li>
-          </ul>
-        </section>
-      </section>
-  )
-}
+      <Header title={"Give feedback"} />
+      <Button
+        title={goodTitle}
+        handleClick={() => setGood((prevState) => prevState + 1)}
+      />
+      <Button
+        title={neutralTitle}
+        handleClick={() => setNeutral((prevState) => prevState + 1)}
+      />
+      <Button
+        title={badTitle}
+        handleClick={() => setBad((prevState) => prevState + 1)}
+      />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        goodTitle={goodTitle}
+        neutralTitle={neutralTitle}
+        badTitle={badTitle}
+      />
+    </section>
+  );
+};
